b080c808b0708869a5c9231bf5ec8ba49dd93f29
Dmytro Yarmak <dmytroy@backbase.com>
2016-07-20 11:24:38 +0300

    Show number of bookmarks in reading list

--- background.js
+++ background.js
@@ -1,5 +1,9 @@
+var BOOKMARKS_FOLDER_ID = '2707';
+
+updateBookmarksCountInBadge();
+
 chrome.browserAction.onClicked.addListener(function() {
-    chrome.bookmarks.getChildren('2707', function(bookmarks) {
+    chrome.bookmarks.getChildren(BOOKMARKS_FOLDER_ID, function(bookmarks) {
         if (bookmarks.length) {
             var randomBookmark = bookmarks[Math.floor(Math.random() * bookmarks.length)]
             chrome.tabs.create({
@@ -8,3 +12,18 @@ chrome.browserAction.onClicked.addListener(function() {
         }
     })
 });
+
+chrome.bookmarks.onCreated.addListener(updateBookmarksCountInBadge);
+chrome.bookmarks.onRemoved.addListener(updateBookmarksCountInBadge);
+chrome.bookmarks.onMoved.addListener(updateBookmarksCountInBadge);
+
+function updateBookmarksCountInBadge() {
+    chrome.bookmarks.getChildren(BOOKMARKS_FOLDER_ID, function(bookmarks) {
+        var bookmarksCount = bookmarks.length;
+        if (bookmarksCount) {
+            chrome.browserAction.setBadgeText({
+                text: bookmarksCount.toString()
+            });
+        }
+    });
+}
